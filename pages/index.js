73459d9bc6d900a81a79459ce9297b4a289779f9
Joe Prathap P J <joeprathappj@gmail.com>
2022-08-09 13:51:12 +0530

    nextjs typesafe

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import Feed from "../components/Feed";
 import Cards from "../components/Cards";
 
 import Router from 'next/router'
+import Link from "next/link";
 export default function Home({ session }) {
   if (session) return (
     <div className="grid place-items-center hover:cursor-pointer hover:bg-blue-500">
@@ -31,9 +32,9 @@ export default function Home({ session }) {
           {/* <Sidebar /> */}
           {/* Feed */}
           <div className="m-20  ml-[300px]  mt-5 mr-1 no-wrap grid md:grid-cols-3 gap-10">
-            <a href="/StudentCertificate"><Cards title="Students Certification" src="https://cdn.searchenginejournal.com/wp-content/uploads/2021/12/google-career-certificates-61bd445f96b29-sej-760x400.png"  /></a>
-            <a href="/Awards"><Cards title="Awards and Rewards" src="https://cdn.pixabay.com/photo/2016/09/16/19/20/trophy-1674911__340.png" /></a>
-            <a href="/Workshops"><Cards title="Workshops" src="https://cdn.pixabay.com/photo/2018/08/29/09/12/business-3639451__340.jpg" /></a>
+            <Link href="/StudentCertificate"><Cards title="Students Certification" src="https://cdn.searchenginejournal.com/wp-content/uploads/2021/12/google-career-certificates-61bd445f96b29-sej-760x400.png"  /></Link>
+            <Link href="/Awards"><Cards title="Awards and Rewards" src="https://cdn.pixabay.com/photo/2016/09/16/19/20/trophy-1674911__340.png" /></Link>
+            <Link href="/Workshops"><Cards title="Workshops" src="https://cdn.pixabay.com/photo/2018/08/29/09/12/business-3639451__340.jpg" /></Link>
 
           </div>
         </div>
@@ -45,9 +46,9 @@ export default function Home({ session }) {
           </h1>
           <div className="flex gap-10 p-4 pl-0 h-[10vh] w-[80vw] mb-[200px]">
           <div className="m-20  ml-[300px]  mt-5 mr-1 no-wrap grid md:grid-cols-3 gap-10">
-            <a href="/StudentCertificate"><Cards title="Students Certification" src="https://cdn.searchenginejournal.com/wp-content/uploads/2021/12/google-career-certificates-61bd445f96b29-sej-760x400.png"  /></a>
-            <a href="/Awards"><Cards title="Awards and Rewards" src="https://cdn.pixabay.com/photo/2016/09/16/19/20/trophy-1674911__340.png" /></a>
-            <a href="/Workshops"><Cards title="Workshops" src="https://cdn.pixabay.com/photo/2018/08/29/09/12/business-3639451__340.jpg" /></a>
+            <Link href="/StudentCertificate"><Cards title="Students Certification" src="https://cdn.searchenginejournal.com/wp-content/uploads/2021/12/google-career-certificates-61bd445f96b29-sej-760x400.png"  /></Link>
+            <Link href="/Awards"><Cards title="Awards and Rewards" src="https://cdn.pixabay.com/photo/2016/09/16/19/20/trophy-1674911__340.png" /></Link>
+            <Link href="/Workshops"><Cards title="Workshops" src="https://cdn.pixabay.com/photo/2018/08/29/09/12/business-3639451__340.jpg" /></Link>
 
           </div>
           </div>
