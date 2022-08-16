e8c622265b6453b80fba413521a8be390d2cf80c
Joe Prathap P J <joeprathappj@gmail.com>
2022-08-16 22:21:20 +0530

    changed

---
+++ components/HeaderIcon.js
@@ -0,0 +1,11 @@
+const HeaderIcon = ({ Icon, active }) => {
+    return (
+        <div className="flex items-center rounded-xl cursor-pointer md:px-10 sm:h-14 md:hover:bg-gray-100 active:border-b-2 active:border-blue-500 group" >
+
+            <Icon className={`h-5 text-gray-500 text-center sm:h-7 mx-auto group-hover:text-blue-500 ${active && "text-blue-500"}`}
+            />
+        </div >
+    );
+}
+
+export default HeaderIcon;
