fafc14bf3f0585e6cda37821a5dc528640ad3a8c
Joe Prathap P J <joeprathappj@gmail.com>
2022-08-17 12:11:26 +0530

    resolved

--- pages/Workshops.tsx
+++ pages/Workshops.tsx
@@ -118,7 +118,7 @@ export default function Workshops() {
 
 {Data && Data.length!==0?(
           Data.map((data)=>{
-            return (<div className=' rounded-md h-[250px] overflow-hidden  justify-center bg-white w-[260px] shadow-md cursor-pointer hover:bg-[#EDEDED] hover:shadow-xl transition-all ease-linear'>
+            return (<div key={data.name}  className=' rounded-md h-[250px] overflow-hidden  justify-center bg-white w-[260px] shadow-md cursor-pointer hover:bg-[#EDEDED] hover:shadow-xl transition-all ease-linear'>
             <div className=''>
                 <Image src={data.imgUrl} width={260} height={200} layout='fixed'/>
                 <div className='flex gap-2 justify-start p-2 font-light text-[1.12rem]'>
