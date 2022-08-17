808519fa3918679845329c10e62b8331a289b56a
Joe Prathap P J <joeprathappj@gmail.com>
2022-08-17 07:04:22 +0530

    Build 1.0

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import App from "../node_modules/next/app";
 import { useAuth } from "../context/AuthContext";
 import { auth } from "../lib/firebase";
+import Head from "../node_modules/next/head";
 
 export default function Login() {
     var [email,setEmail]=useState("")
@@ -45,6 +46,19 @@ export default function Login() {
 
   return (
     <form onSubmit={(e)=>(handleSubmit(e))}>
+         <Head>
+      
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="theme-color" content="#ffffff" />
+    
+            <title>
+              Students Portal
+            </title>
+          </Head>
     <div>
     <Image className="relative" src="/srecbg.jpg"  layout="fill"/>
     <div className="relative flex items-center justify-center w-screen h-screen">
