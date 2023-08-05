59e537a1de8398ccea95b43ae18b406a955b7216
Joe Prathap P J <joeprathappj@gmail.com>
2023-08-05 10:16:27 +0530

    nfc

--- pages/nfc.jsx
+++ pages/nfc.jsx
@@ -68,7 +68,9 @@ function Nfc() {
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen gap-2 h-max bg-red'>
-        {tagData && tagData.length>0 && tagData.map((data,idx)=>{
+        
+        {tagData && tagData.map((data,idx)=>{
+            console.log(tagData)
             return(
                 <h1 key={idx}>{JSON.stringify(data)}</h1>
             )
